20afb2ab645a0c8db7322092fd51cd8734b5458e
gyleodhis <gyleodhis@outlook.com>
2021-11-14 21:46:44 +0300

    Updated data source

--- data.py
+++ data.py
@@ -3,14 +3,14 @@ from datetime import datetime as dt
 from functools import reduce
 
 
-# covid_data = 'https://covid.ourworldindata.org/data/owid-covid-data.csv'
-covid_data = 'assets/covid_data.csv'  # offline csv version
+covid_data = 'https://covid.ourworldindata.org/data/owid-covid-data.csv'
+# covid_data = 'assets/covid_data.csv'  # offline csv version
 
 df_covid_data_v1 = pd.read_csv(covid_data)
 df_covid_data_v1['date'] = pd.to_datetime(df_covid_data_v1['date'])  # converting date column to type date
 
 """Covicd 19 Daily vaccinations data"""
-# vax_url = 'https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/vaccinations-by-manufacturer.csv'
+vax_url = 'https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/vaccinations-by-manufacturer.csv'
 """Offline version"""
-vax_url = 'assets/vaccines.csv'
+# vax_url = 'assets/vaccines.csv'
 
